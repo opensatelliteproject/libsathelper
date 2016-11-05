dc9aac0ea464487b9034dbd31e0c8f88f3d16952
Lucas Teske <lucas@teske.net.br>
2016-11-05 00:15:43 -0200

    Added Debug/Release folders with makefiles

---
+++ Debug/src/subdir.mk
@@ -0,0 +1,30 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/correlator.cpp \
+../src/reedsolomon.cpp \
+../src/viterbi27.cpp 
+
+OBJS += \
+./src/correlator.o \
+./src/reedsolomon.o \
+./src/viterbi27.o 
+
+CPP_DEPS += \
+./src/correlator.d \
+./src/reedsolomon.d \
+./src/viterbi27.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -std=c++11 -I"/home/lucas/Works/OpenSatelliteProject/open-satellite-project/SatHelper/includes" -I"/home/lucas/Works/OpenSatelliteProject/open-satellite-project/SatHelper/includes/exceptions" -O0 -g3 -Wall -c -fmessage-length=0 -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
