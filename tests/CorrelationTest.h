de410fc2525c905cd224d9f9ae11ff3b9da547f5
Lucas Teske <lucas@teske.net.br>
2017-01-25 07:39:00 -0200

    Added Correlation Test

--- tests/CorrelationTest.h
+++ tests/CorrelationTest.h
@@ -13,11 +13,6 @@
 namespace SatHelper {
 
     class CorrelationTest: public BaseTest {
-    private:
-        static const uint8_t word[];
-        static const uint8_t data[];
-        static const uint32_t correlation[];
-
     public:
         std::string TestName() const override { return std::string("Correlation Test"); }
         bool RunTest() override;
