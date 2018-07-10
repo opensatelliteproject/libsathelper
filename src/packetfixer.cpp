d65d3e3a234e233a500ce85828c9ec551017a94f
Lucas Teske <lucas@teske.net.br>
2018-07-10 03:43:08 -0300

    Fixed PacketFixer and PacketFixer Tests for 90/270/IQ Rotations

--- src/packetfixer.cpp
+++ src/packetfixer.cpp
@@ -7,50 +7,23 @@
 
 #include <SatHelper/packetfixer.h>
 
-
 using namespace SatHelper;
 
-bool PacketFixer::initialized = false;
-uint8_t PacketFixer::deg90LUT[256];
-uint8_t PacketFixer::iqInvertLUT[256];
-
-void PacketFixer::initializeLUT() {
-    for (int i = 0; i < 256; i++) {
-        // Phase Ambiquity Mapping for every two bits:
-        //00 90
-        //ab ab
-        //00 10
-        //01 00
-        //11 01
-        //10 11
-
-        // (90deg a) = ~ (0deg b)
-        // (90deg b) =   (0deg a)
-
-        deg90LUT[i] = ((i & 0x55) ^ 0x55) << 1 | (i & 0xAA) >> 1;
-
-        // IQ Inversion. Swap every two bits
-        iqInvertLUT[i] = (i & 0x55) << 1 | (i & 0xAA) >> 1;
+void PacketFixer::fixPacket(uint8_t *data, uint32_t length, SatHelper::PhaseShift phaseShift, bool iqInversion) {
+  if (iqInversion || phaseShift != SatHelper::PhaseShift::DEG_0) {
+    for (uint32_t i = 0; i < length; i+=2) {
+      if (iqInversion) {
+        uint8_t x = data[i+1];
+        data[i+1] = data[i];
+        data[i] = x;
+      }
+      if (phaseShift == SatHelper::PhaseShift::DEG_90 || phaseShift == SatHelper::PhaseShift::DEG_270) {
+        Rotate(&data[i]);
+      }
+      if (phaseShift == SatHelper::PhaseShift::DEG_180 || phaseShift == SatHelper::PhaseShift::DEG_270) {
+        data[i] ^= 0xFF;
+        data[i+1] ^= 0xFF;
+      }
     }
-    initialized = true;
+  }
 }
-
-void PacketFixer::fixPacket(uint8_t *data, uint32_t length, PhaseShift phaseShift, bool iqInversion) {
-    for (uint32_t i = 0; i < length; i++) {
-        // Process IQ Inversion
-        if (iqInversion) {
-            data[i] = iqInvertLUT[data[i]];
-        }
-
-        // 270 degrees is actually 90 degrees inverted
-        if (phaseShift == DEG_90 || phaseShift == DEG_270) {
-            data[i] = deg90LUT[data[i]];
-        }
-
-        // Invert
-        if (phaseShift == DEG_180 || phaseShift == DEG_270) {
-            data[i] ^= 0xFF;
-        }
-    }
-}
-
